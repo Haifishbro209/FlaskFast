a512a9356c30cff5ff6ebaf836d88b255b4dfcb7
haifishbro209 <kaloyannikolov016@gmail.com>
2025-10-01 07:13:07 +0200

    .env

--- .dev/init_project.py
+++ .dev/init_project.py
@@ -1,4 +1,10 @@
-from .create_env import extract_oauth_creds_to_dotenv
+import sys
+import os
+sys.path.insert(0, os.path.dirname(__file__))
 
+from create_env import extract_oauth_creds_to_dotenv
+
+print("Let's start by making the .env file")
+extract_oauth_creds_to_dotenv()
 print("Let's start by making the .env file")
 extract_oauth_creds_to_dotenv()
