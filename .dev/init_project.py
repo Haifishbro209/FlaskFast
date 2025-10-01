c709bcc4a9ba1d878cdf4a5d9b34536c4d960db4
haifishbro209 <kaloyannikolov016@gmail.com>
2025-10-01 07:11:43 +0200

    Works

--- .dev/init_project.py
+++ .dev/init_project.py
@@ -1,8 +1,4 @@
-import sys
-import os
-sys.path.insert(0, os.path.dirname(__file__))
-
-from create_env import extract_oauth_creds_to_dotenv
+from .create_env import extract_oauth_creds_to_dotenv
 
 print("Let's start by making the .env file")
 extract_oauth_creds_to_dotenv()
